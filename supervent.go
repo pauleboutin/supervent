d300803d1964c531da4b7c139d081b41f0d1d39c
pboutin-ai <pboutin@axiom.co>
2024-10-21 23:17:04 -0700

    Go versiom now supports constraint parameters as documented

--- supervent.go
+++ supervent.go
@@ -1,260 +1,335 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"math"
+	"log"
 	"math/rand"
-	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
-	"syscall"
 	"time"
 
-	"gopkg.in/yaml.v2"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"github.com/valyala/fasthttp"
 )
 
-const (
-	DEFAULT_BATCH_SIZE = 100
-)
+const DEFAULT_BATCH_SIZE = 100
 
 type Config struct {
 	Sources []SourceConfig `json:"sources"`
 }
 
 type SourceConfig struct {
-	Name            string           `json:"name"`
 	Vendor          string           `json:"vendor"`
-	EventFormat     string           `json:"event_format"`
 	TimestampFormat string           `json:"timestamp_format"`
 	Fields          map[string]Field `json:"fields"`
 }
 
 type Field struct {
-	Type        string      `json:"type"`
-	Formats     []string    `json:"formats,omitempty"`
-	Constraints Constraints `json:"constraints,omitempty"`
+	Type          string                 `json:"type"`
+	AllowedValues []string               `json:"allowed_values,omitempty"`
+	Weights       []float64              `json:"weights,omitempty"`
+	Constraints   map[string]interface{} `json:"constraints,omitempty"`
+	Distribution  string                 `json:"distribution,omitempty"`
+	Mean          float64                `json:"mean,omitempty"`
+	Stddev        float64                `json:"stddev,omitempty"`
+	Lambda        float64                `json:"lambda,omitempty"`
+	S             float64                `json:"s,omitempty"`
+	Alpha         float64                `json:"alpha,omitempty"`
+	Format        string                 `json:"format,omitempty"`
+	Formats       []string               `json:"formats,omitempty"`
 }
 
-type Constraints struct {
-	Min           string   `json:"min,omitempty"`
-	Max           string   `json:"max,omitempty"`
-	AllowedValues []string `json:"allowed_values,omitempty"`
+type EventGenerator struct {
+	Dataset      string
+	APIKey       string
+	URL          string
+	BatchSize    int
+	Batch        []map[string]interface{}
+	PostgresConn *sql.DB
 }
 
-type AxiomConfig struct {
-	APIKey        string `yaml:"AXIOM_API_KEY"`
-	Dataset       string `yaml:"AXIOM_DATASET"`
-	HTTPBatchSize int    `yaml:"HTTP_BATCH_SIZE,omitempty"`
-}
-
-func loadConfig(filePath string) (Config, error) {
-	var config Config
-	file, err := os.ReadFile(filePath)
-	if err != nil {
-		return config, err
+func NewEventGenerator(dataset, apiKey string, batchSize int, postgresConfig *PostgresConfig) *EventGenerator {
+	eg := &EventGenerator{
+		Dataset:   dataset,
+		APIKey:    apiKey,
+		URL:       fmt.Sprintf("https://api.axiom.co/v1/datasets/%s/ingest", dataset),
+		BatchSize: batchSize,
+		Batch:     make([]map[string]interface{}, 0, batchSize),
 	}
-	err = json.Unmarshal(file, &config)
-	return config, err
-}
 
-func loadAxiomConfig(filePath string) (AxiomConfig, error) {
-	var config AxiomConfig
-	file, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return config, err
+	if postgresConfig != nil {
+		connStr := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=disable",
+			postgresConfig.Host, postgresConfig.Port, postgresConfig.DBName, postgresConfig.User, postgresConfig.Password)
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatalf("Failed to connect to PostgreSQL: %v", err)
+		}
+		eg.PostgresConn = db
 	}
-	err = yaml.Unmarshal(file, &config)
-	return config, err
+
+	return eg
 }
 
-func generateEvent(source SourceConfig) map[string]interface{} {
-	event := map[string]interface{}{"source": source.Vendor}
-	for field, details := range source.Fields {
-		switch details.Type {
-		case "datetime":
-			switch source.TimestampFormat {
-			case "UTC":
-				event[field] = time.Now().UTC().Format(details.Formats[0])
-			case "ISO":
-				event[field] = time.Now().Format(time.RFC3339)
-			case "Unix":
-				event[field] = time.Now().Unix()
-			case "RFC3339":
-				event[field] = time.Now().Format(time.RFC3339)
-			default:
-				event[field] = time.Now().Format(source.TimestampFormat)
-			}
-		case "string":
-			if len(details.Constraints.AllowedValues) > 0 {
-				event[field] = details.Constraints.AllowedValues[rand.Intn(len(details.Constraints.AllowedValues))]
-			} else {
-				if field == "message" {
-					if len(details.Formats) > 0 {
-						selectedFormat := details.Formats[rand.Intn(len(details.Formats))]
-						event[field] = strings.TrimSpace(selectedFormat)
-					} else {
-						event[field] = "No message format available"
-					}
-				} else {
-					event[field] = generateRandomUsername()
-				}
-			}
-		case "int":
-			min := 0
-			max := 100
-			if details.Constraints.Min != "" {
-				min, _ = strconv.Atoi(details.Constraints.Min)
-			}
-			if details.Constraints.Max != "" {
-				max, _ = strconv.Atoi(details.Constraints.Max)
-			}
-			event[field] = rand.Intn(max-min) + min
+func (eg *EventGenerator) Emit(record map[string]interface{}) {
+	record["_time"] = time.Now().UTC().Format(time.RFC3339)
+	eg.Batch = append(eg.Batch, record)
+	if len(eg.Batch) >= eg.BatchSize {
+		eg.SendBatch()
+	}
+}
+
+func (eg *EventGenerator) SendBatch() {
+	if len(eg.Batch) == 0 {
+		return
+	}
+
+	fmt.Println("sending batch")
+	headers := map[string]string{
+		"Content-Type":  "application/json",
+		"Authorization": fmt.Sprintf("Bearer %s", eg.APIKey),
+	}
+
+	body, err := json.Marshal(eg.Batch)
+	if err != nil {
+		log.Fatalf("Failed to marshal batch: %v", err)
+	}
+
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI(eg.URL)
+	req.Header.SetMethod("POST")
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	req.SetBody(body)
+
+	resp := fasthttp.AcquireResponse()
+	client := &fasthttp.Client{}
+	if err := client.Do(req, resp); err != nil {
+		log.Printf("Failed to send batch: %v", err)
+	} else if resp.StatusCode() != fasthttp.StatusOK {
+		log.Printf("Failed to send batch: %d", resp.StatusCode())
+	} else {
+		fmt.Println("Batch sent successfully")
+	}
+
+	if eg.PostgresConn != nil {
+		eg.SendToPostgres(eg.Batch)
+	}
+
+	eg.Batch = eg.Batch[:0]
+	fasthttp.ReleaseRequest(req)
+	fasthttp.ReleaseResponse(resp)
+}
+
+func (eg *EventGenerator) SendToPostgres(batch []map[string]interface{}) {
+	for _, record := range batch {
+		columns := make([]string, 0, len(record))
+		values := make([]interface{}, 0, len(record))
+		for k, v := range record {
+			columns = append(columns, k)
+			values = append(values, v)
+		}
+		insertStatement := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)",
+			eg.Dataset, join(columns, ","), placeholders(len(values)))
+		_, err := eg.PostgresConn.Exec(insertStatement, values...)
+		if err != nil {
+			log.Printf("Failed to insert record into PostgreSQL: %v", err)
 		}
 	}
+}
+
+func join(strs []string, sep string) string {
+	result := ""
+	for i, s := range strs {
+		if i > 0 {
+			result += sep
+		}
+		result += s
+	}
+	return result
+}
+
+func placeholders(n int) string {
+	result := ""
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			result += ","
+		}
+		result += fmt.Sprintf("$%d", i+1)
+	}
+	return result
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     int
+	DBName   string
+	User     string
+	Password string
+}
+
+func loadConfig(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func generateEvent(sourceConfig SourceConfig) map[string]interface{} {
+	event := map[string]interface{}{
+		"source": sourceConfig.Vendor,
+	}
+	for field, details := range sourceConfig.Fields {
+		switch details.Type {
+		case "datetime":
+			event[field] = generateDatetime(details, sourceConfig.TimestampFormat)
+		case "string":
+			event[field] = generateString(details)
+		case "int":
+			event[field] = generateInt(details)
+		}
+	}
+	fmt.Println(event) // Debug statement to print the complete event
 	return event
 }
 
-// Removed unused function generateRandomIPAddress
-
-func generateRandomUsername() string {
-	usernames := []string{
-		"john_doe", "jane_smith", "mohamed_ali", "li_wei", "maria_garcia",
-		"yuki_tanaka", "olga_petrov", "raj_kumar", "fatima_zahra", "chen_wang",
-		"ahmed_hassan", "isabella_rossi", "david_jones", "sophia_martinez", "emily_clark",
-		"noah_brown", "mia_wilson", "lucas_miller", "oliver_davis", "ava_moore",
-		"ethan_taylor", "amelia_anderson", "james_thomas", "harper_jackson", "benjamin_white",
-		"liam_johnson", "emma_rodriguez", "william_lee", "sophia_kim", "mason_martin",
-		"elijah_hernandez", "logan_lopez", "alexander_gonzalez", "sebastian_perez", "daniel_hall",
-		"matthew_young", "henry_king", "jack_wright", "levi_scott", "isaac_green",
-		"gabriel_baker", "julian_adams", "jayden_nelson", "lucas_carter", "anthony_mitchell",
-		"grayson_perez", "dylan_roberts", "leo_turner", "jaxon_phillips", "asher_campbell",
-		"ananya_sharma", "arjun_patel", "priya_singh", "vikram_gupta", "neha_verma",
-		"sanjay_rana", "deepika_kapoor", "ravi_mehta", "sara_khan", "manoj_joshi",
-		"željko_ivanović", "šime_šarić", "đorđe_đorđević", "čedomir_čolić", "žana_živković",
-		"miloš_milošević", "ana_marija", "ivan_ivanov", "petar_petrov", "nikola_nikolić",
-		"marta_novak", "katarina_kovač", "tomaž_tomažič", "matej_matejić", "vanja_vuković",
-		"dragana_dimitrijević", "bojan_bojović", "milica_milovanović", "stefan_stefanović", "vanja_vasić",
-		"igor_ilić", "jelena_jovanović", "marko_marković", "tanja_tomić", "zoran_zorić",
+func generateDatetime(details Field, timestampFormat string) string {
+	switch timestampFormat {
+	case "UTC":
+		return time.Now().UTC().Format(details.Format)
+	case "ISO":
+		return time.Now().Format(time.RFC3339)
+	case "Unix":
+		return fmt.Sprintf("%d", time.Now().Unix())
+	case "RFC3339":
+		return time.Now().Format(time.RFC3339)
+	default:
+		return time.Now().Format(details.Format)
 	}
+}
 
-	// Generate weights using Zipf's law
-	weights := make([]float64, len(usernames))
-	s := 1.07 // Zipf's law parameter
-	for i := range weights {
-		weights[i] = 1.0 / math.Pow(float64(i+1), s)
+func generateString(details Field) string {
+	if len(details.AllowedValues) > 0 {
+		if len(details.Weights) > 0 {
+			return weightedChoice(details.AllowedValues, details.Weights)
+		}
+		return details.AllowedValues[rand.Intn(len(details.AllowedValues))]
 	}
+	if len(details.Formats) > 0 {
+		selectedFormat := details.Formats[rand.Intn(len(details.Formats))]
+		return fmt.Sprintf(selectedFormat, time.Now().Format(details.Format))
+	}
+	return uuid.New().String()
+}
 
-	// Normalize weights
+func generateInt(details Field) int {
+	min := int(details.Constraints["min"].(float64))
+	max := int(details.Constraints["max"].(float64))
+	switch details.Distribution {
+	case "uniform":
+		return rand.Intn(max-min+1) + min
+	case "normal":
+		return int(rand.NormFloat64()*details.Stddev + details.Mean)
+	case "exponential":
+		return int(rand.ExpFloat64() / details.Lambda)
+	case "zipfian":
+		return int(randZipf(details.S))
+	case "long_tail":
+		return int(randPareto(details.Alpha))
+	case "random":
+		return rand.Intn(max-min+1) + min
+	default:
+		return rand.Intn(max-min+1) + min
+	}
+}
+
+func weightedChoice(values []string, weights []float64) string {
 	totalWeight := 0.0
 	for _, weight := range weights {
 		totalWeight += weight
 	}
-	for i := range weights {
-		weights[i] /= totalWeight
-	}
-
-	// Select a username based on the weights
-	r := rand.Float64()
+	r := rand.Float64() * totalWeight
 	for i, weight := range weights {
-		r -= weight
-		if r <= 0 {
-			return usernames[i]
+		if r < weight {
+			return values[i]
 		}
+		r -= weight
 	}
-	return usernames[len(usernames)-1]
+	return values[len(values)-1]
 }
 
-func sendBatch(events []map[string]interface{}, dataset, apiKey string) error {
-	url := fmt.Sprintf("https://api.axiom.co/v1/datasets/%s/ingest", dataset)
-	//fmt.Println("Sending batch of", len(events), "events to", url, "key", apiKey)
-	body, err := json.Marshal(events)
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+func randZipf(s float64) float64 {
+	return rand.ExpFloat64() / s
+}
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to send batch: %s", resp.Status)
-	}
-	return nil
+func randPareto(alpha float64) float64 {
+	return rand.ExpFloat64() / alpha
 }
 
 func main() {
-	// Define the --config flag
 	configPath := flag.String("config", "config.json", "Path to the configuration file")
+	dataset := flag.String("dataset", "", "Axiom dataset name")
+	apiKey := flag.String("api_key", "", "Axiom API key")
+	batchSize := flag.Int("batch_size", DEFAULT_BATCH_SIZE, "Batch size for HTTP requests")
+	postgresHost := flag.String("postgres_host", "", "PostgreSQL host")
+	postgresPort := flag.Int("postgres_port", 5432, "PostgreSQL port")
+	postgresDB := flag.String("postgres_db", "", "PostgreSQL database name")
+	postgresUser := flag.String("postgres_user", "", "PostgreSQL user")
+	postgresPassword := flag.String("postgres_password", "", "PostgreSQL password")
 	flag.Parse()
 
-	// Load the configuration file
+	if *dataset == "" || *apiKey == "" {
+		log.Fatal("dataset and api_key are required")
+	}
+
 	config, err := loadConfig(*configPath)
 	if err != nil {
-		fmt.Println("Error loading config:", err)
-		return
+		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Load the Axiom configuration
-	axiomConfig, err := loadAxiomConfig("axiom_config.yaml")
-	if err != nil {
-		fmt.Println("Error loading Axiom config:", err)
-		return
+	var postgresConfig *PostgresConfig
+	if *postgresHost != "" && *postgresDB != "" && *postgresUser != "" && *postgresPassword != "" {
+		postgresConfig = &PostgresConfig{
+			Host:     *postgresHost,
+			Port:     *postgresPort,
+			DBName:   *postgresDB,
+			User:     *postgresUser,
+			Password: *postgresPassword,
+		}
 	}
 
-	batchSize := DEFAULT_BATCH_SIZE
-	if axiomConfig.HTTPBatchSize > 0 {
-		batchSize = axiomConfig.HTTPBatchSize
-	}
+	eventGenerator := NewEventGenerator(*dataset, *apiKey, *batchSize, postgresConfig)
 
-	var allEvents []map[string]interface{}
-
-	// Set up signal handling to gracefully exit on ^C
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
 	go func() {
 		<-sigChan
 		fmt.Println("Received interrupt signal, sending remaining events...")
-		if len(allEvents) > 0 {
-			err = sendBatch(allEvents, axiomConfig.Dataset, axiomConfig.APIKey)
-			if err != nil {
-				fmt.Println("Error sending batch:", err)
-			} else {
-				// fmt.Println("Batch sent successfully")
-			}
+		eventGenerator.SendBatch()
+		if eventGenerator.PostgresConn != nil {
+			eventGenerator.PostgresConn.Close()
 		}
 		os.Exit(0)
 	}()
 
-	// Generate events in a round-robin fashion indefinitely
 	for {
 		for _, source := range config.Sources {
 			event := generateEvent(source)
-			allEvents = append(allEvents, event)
-			if len(allEvents) >= batchSize {
-				err = sendBatch(allEvents, axiomConfig.Dataset, axiomConfig.APIKey)
-				if err != nil {
-					fmt.Println("Error sending batch:", err)
-				} else {
-					// fmt.Println("Batch sent successfully")
-				}
-				allEvents = allEvents[:0] // Clear the slice
-			}
+			eventGenerator.Emit(event)
 		}
+		time.Sleep(1 * time.Second) // Adjust the sleep duration as needed
 	}
 }
